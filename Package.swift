eab3c07c4e01184d9f6ebfdfad1a29f0d5147132
Fabio Carvalho <f.carvalho@swordhealth.com>
2023-04-03 17:00:29 +0100

    Added Swift exportable

--- Package.swift
+++ Package.swift
@@ -9,7 +9,8 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "BleSDK",
-            targets: ["BleSDK"]),
+            targets: ["BleSDK"]
+		),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -19,11 +20,18 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
 		// Targets can depend on other targets in this package, and on products in packages this package depends on.
 		.target(
-			name: "BleSDK",
+			name: "BleSDK-ObjC",
 			dependencies: [],
-			publicHeadersPath: "include"),
-		.testTarget(
-			name: "BleSDKTests",
-			dependencies: ["BleSDK"]),
+			path: "BleSDK",
+			exclude: ["Swift"],
+			cSettings: [
+				.headerSearchPath("Internal"),
+			]
+		),
+		.target(
+			name: "BleSDK",
+			dependencies: ["BleSDK-ObjC"],
+			path: "Swift"
+		),
 	]
 )
