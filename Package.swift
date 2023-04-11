216591a821662bbda898d946cb280b1088f2d2ce
Fabio Carvalho <f.carvalho@swordhealth.com>
2023-04-11 16:32:46 +0100

    Third try

--- Package.swift
+++ Package.swift
@@ -3,36 +3,17 @@
 
 import PackageDescription
 
-#if targetEnvironment(simulator)
-
 let package = Package(
 	name: "MoveWearableSDK",
 	platforms: [.iOS(.v14)],
 	products: [
-		.library(name: "MoveWearableSDK", targets: ["Simulator"]),
+		.library(name: "MoveWearableSDK", targets: ["MoveWearableSDK", "BleSDK"]),
 	],
 	dependencies: [
 	],
 	targets: [
-		.target(name: "Simulator")
-	]
-)
-
-#else
-
-let package = Package(
-    name: "MoveWearableSDK",
-	platforms: [.iOS(.v14)],
-    products: [
-        .library(name: "MoveWearableSDK", targets: ["MoveWearableSDK", "BleSDK"]),
-    ],
-    dependencies: [
-    ],
-    targets: [
 		.target(name: "Internal", dependencies: [.target(name: "MoveWearableSDK")]),
 		.target(name: "MoveWearableSDK", path: "MoveWearableSDK/Wrapper"),
 		.binaryTarget(name: "BleSDK", path: "artifacts/BleSDK.xcframework"),
-    ]
+	]
 )
-
-#endif
