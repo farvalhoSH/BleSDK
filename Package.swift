430ad3b97b5e2f92a4f24d93e14ae33c63915e6d
Fabio Carvalho <f.carvalho@swordhealth.com>
2024-05-16 14:57:10 +0100

    Bump iOS version

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
 	name: "MoveWearableSDK",
-	platforms: [.iOS(.v14)],
+	platforms: [.iOS(.v15)],
 	products: [
 		.library(name: "MoveWearableSDK", targets: ["MoveWearableSDK", "BleSDK"]),
 	],
